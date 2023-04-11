3e9b9ab7bf5f25e6b9a417980ad0716f5aa3f127
Eduardo-Cerqueira <eduardoccerqueira778@gmail.com>
2023-04-11 15:49:06 +0200

    server start

---
+++ server/server.py
@@ -0,0 +1,52 @@
+import socket
+
+class user:
+  def __init__(self, username, hostname):
+    self.username = username
+    self.hostname = hostname
+
+def server_program():
+    # get the hostname
+    host = socket.gethostname()
+    port = 5000  # initiate port no above 1024
+
+    server_socket = socket.socket()  # get instance
+    # look closely. The bind() function takes tuple as argument
+    server_socket.bind((host, port))  # bind host address and port together
+
+    # configure how many client the server can listen simultaneously
+    server_socket.listen(2)
+    conn, address = server_socket.accept()  # accept new connection
+    print("Connection from: " + str(address))
+    while True:
+        # receive data stream. it won't accept data packet greater than 1024 bytes
+        data = conn.recv(1024).decode()
+        if not data:
+            # if data is not received break
+            break
+        print("from connected user: " + str(data))
+        data = input(' -> ')
+        conn.send(data.encode())  # send data to the client
+
+    conn.close()  # close the connection
+
+def checkUser(users_array, username):
+    for user in users_array:
+        if (user.username == username):
+            return print({'status': 500, 'err': 'Username already exists !'})
+    return print({'status': 200, 'err': "Username doesn't exist !"})
+
+
+def addUser(users_array, username, hostname):
+    return users_array.append(user(username, hostname))
+
+def removeUser(users_array, username):
+    i = 0
+    for user in users_array:
+        if (user.username == username):
+            return users_array.pop(i)
+        i+=1
+
+if __name__ == '__main__':
+    users = []
+    server_program()
