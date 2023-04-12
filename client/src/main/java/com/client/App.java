82fedcfb1b078abb2fe97605a11206d0163e0582
Eduardo-Cerqueira <eduardoccerqueira778@gmail.com>
2023-04-12 09:34:28 +0200

    client basic setup

---
+++ client/src/main/java/com/client/App.java
@@ -0,0 +1,35 @@
+package com.client;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ConnectException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.Scanner;
+import org.json.*;
+
+public final class App {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter username : ");
+        String username = scanner.nextLine();
+        System.out.println("Enter hostname : ");
+        String hostnameserver = scanner.nextLine();
+        scanner.close();
+        
+        JSONObject username_json = new JSONObject();
+        username_json.put("username", username);
+        String hostname = InetAddress.getLocalHost().getHostName();
+        username_json.put("hostname", hostname);
+
+        try {
+            Socket s = new Socket(hostnameserver, 5000);
+            PrintWriter username_send = new PrintWriter(s.getOutputStream());
+            username_send.println(username_json);
+            username_send.flush();
+            s.close();
+        } catch (ConnectException e) {
+            System.out.println("Error : Serveur doesn't exist or is offline !");
+        }
+    }
+}
