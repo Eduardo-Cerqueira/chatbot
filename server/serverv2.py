f50c81e263d02f98ee2daa6acd2381a4716837e4
Eduardo-Cerqueira <eduardoccerqueira778@gmail.com>
2023-04-12 20:29:41 +0200

    add logs

--- server/serverv2.py
+++ server/serverv2.py
@@ -1,5 +1,52 @@
-import socket
+import socket, os
+from datetime import datetime
 from _thread import start_new_thread
+
+class User:
+  def __init__(self, username, hostname, address, conn):
+    self.username = username
+    self.hostname = hostname
+    self.address = address
+    self.conn = conn
+
+## Logs
+def time():
+    #current time for logs
+    now = datetime.now()
+    dt_string = now.strftime("%d/%m/%Y | %H:%M:%S")
+    return dt_string
+
+
+def log_message(path, message_log):
+    #Logs during server execution
+    with open(os.path.join(path), "a") as file_logs:
+        file_logs.write(f"{time()} : {message_log}\n")
+
+def log_file_manage(log_path):
+    #Create log file at start of main if doesn't exist
+    is_file = os.path.isfile(log_path)
+    if is_file is False:
+        with open(os.path.join(log_path), "x") as file_logs:
+            file_logs.write(f"{time()} : Server started\n")
+            file_logs.close()
+    else:
+        with open(os.path.join(log_path), "a") as file_logs:
+            file_logs.write(f"{time()} : Server started\n")
+            file_logs.close()
+
+def startServer(port):
+    log_file_manage("server/server.log")
+    host = socket.gethostname()
+    print("[+] Server address at", host)
+
+    server_socket = socket.socket()
+    print("[-] Socket Created")
+    server_socket.bind((host, port))
+    print("[-] Socket Bound to port 5000")
+    server_socket.listen(10)
+    print("Listening...")
+
+    return server_socket
 
 def sendOthers(conn_user, data, array):
     try:
@@ -33,22 +80,14 @@ def client_thread(conn, users):
     conn.close()
 
 if __name__ == '__main__':
-    host = socket.gethostname()
-    print("[+] Server address at", host)
-
-    server_socket = socket.socket()
-    print("[-] Socket Created")
-    server_socket.bind((host, 5000))
-    print("[-] Socket Bound to port 5000")
-    server_socket.listen(10)
-    print("Listening...")
-
+    server_socket = startServer(5000)
     users = []
 
     while True:
         conn, address = server_socket.accept()
         print("[-] Connected to " + address[0] + ":" + str(address[1]))
-        conmsg = "[-]" + address[0] + ":" + str(address[1]) + " joined !"
+        conmsg = "[-] " + address[0] + ":" + str(address[1]) + " joined !"
+        log_message("server/server.log", conmsg)
         sendOthers(conn, conmsg, users)
         start_new_thread(newUser, (conn, address, users,))
         start_new_thread(client_thread, (conn, users,))
