c868c5a31e5bc7b68c3c065d2029bf547b13d28e
Kisat0 <ghanembenyoucef@gmail.com>
2023-04-14 00:44:24 +0200

    Update interface and add thread

---
+++ Client/src/main/java/com/example/client/ApplicationController.java
@@ -0,0 +1,40 @@
+package com.example.client;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ApplicationController {
+    @FXML
+    private TextField nicknameField;
+
+    @FXML
+    private Label errorLabel;
+
+    @FXML
+    protected void login() throws IOException {
+        if (!nicknameField.getText().isEmpty() || !nicknameField.getText().isBlank()){
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Menu.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+
+        MenuController controller = loader.getController();
+        controller.setNickname(nicknameField.getText());
+
+        Stage stage = (Stage) nicknameField.getScene().getWindow();
+
+        stage.setScene(scene);
+        stage.show();
+    }
+        else{
+            errorLabel.setText("Please enter a correct nickname before !");
+            errorLabel.setVisible(true);
+        }
+    }
+}
